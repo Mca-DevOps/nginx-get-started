dd6ae317b7a25b167abc2c62e2c85a1cc8ffcb25
frimpong-adotri-01 <goldmalcom43@gmail.com>
2025-03-12 17:55:26 +0100

    [FEAT]: Modification of main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import uvicorn
 from backend.backend_app import app
-from backend import backend_app
 
 
 if __name__ == "__main__":
     ## Running ASGI Server
-    uvicorn.run(f"{backend_app}:{app}", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run(f"main:app", host="0.0.0.0", port=3000, reload=True)
