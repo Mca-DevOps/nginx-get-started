a1d58e8bdf7ba5afadef746f1bca3947ba451e13
frimpong-adotri-01 <goldmalcom43@gmail.com>
2025-03-14 02:32:45 +0100

    [FEAT]: Modification of main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import uvicorn
 from backend.backend_app import app
+import os
 
 
 if __name__ == "__main__":
     ## Running ASGI Server
+    print(f"""\x1b[6;30;42m APP {os.environ["ID"]} is running on port 3000 \x1b[0m""")
     uvicorn.run(f"main:app", host="0.0.0.0", port=3000)
